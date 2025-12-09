ca332e4e5e93885612b8aef67d0a85a40ac4ca61
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-12-09 22:21:18 +0330

    Create Function_in_function.py
    I use a Function in function and i use that in that function

---
+++ Function_in_function.py
@@ -0,0 +1,18 @@
+"""I write a Functions in function and i use it in this function"""
+
+
+def Calculste(a, b, what_do_you_want):
+    def add(a, b):
+        return a + b
+
+    def zarb(a, b):
+        return a * b
+
+    if what_do_you_want == "add":
+        return add(a, b)
+    if what_do_you_want == "zarb":
+        return zarb(a, b)
+
+
+result = Calculste(3, 5, "zarb")
+print(result)
